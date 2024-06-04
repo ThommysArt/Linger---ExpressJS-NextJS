45aa00b196120ab07216dfad0a80cefe63beebe6
Kidkiller001 <138249186+tobias0klug@users.noreply.github.com>
2024-06-04 06:27:53 +0100

    finishedQuiz.ts

---
+++ Server/src/routes/finishedQuiz.ts
@@ -0,0 +1,12 @@
+import { Router } from 'express';
+import FinishedQuizController from './finishedQuizController';
+
+const router = Router();
+
+// Define routes and associate them with controller methods
+router.post('/finishedQuiz', FinishedQuizController.addFinishedQuiz);
+router.get('/finishedQuiz/:userId', FinishedQuizController.getFinishedQuizzes);
+router.delete('/finishedQuiz/:id', FinishedQuizController.deleteFinishedQuiz);
+router.put('/finishedQuiz/:id/score', FinishedQuizController.updateScore);
+
+export default router;
