b56a24ee41389086b74beaf828876338be071613
ThommysArt <keabou.thomson@ictuniversity.edu.cm>
2024-06-10 19:36:02 +0100

    fixed question route

--- Server/src/routes/question.ts
+++ Server/src/routes/question.ts
@@ -1,11 +1,11 @@
 import { Router } from 'express';
-import { createQuestion, getQuestion, getQuizQuestions, deleteQuestion } from '../controllers/questionController'; // Adjust the import path as needed
+import QuestionController from '../controllers/questionController';
 
 const router = Router();
 
-router.post('/questions', createQuestion);
-router.get('/questions/:id', getQuestion);
-router.get('/quizzes/:quizid/questions', getQuizQuestions);
-router.delete('/questions/:id', deleteQuestion);
+router.post('/questions', QuestionController.createQuestion);
+router.get('/questions/:id', QuestionController.getQuestion);
+router.get('/quizzes/:quizid/questions', QuestionController.getQuizQuestions);
+router.delete('/questions/:id', QuestionController.deleteQuestion);
 
 export default router
